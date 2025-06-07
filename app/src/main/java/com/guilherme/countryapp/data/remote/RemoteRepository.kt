f6e06a56be798321bed97ca004e3b8556befe6d0
Guilherme Sousa <guilhermevcsousa@hotmail.com>
2025-06-07 16:18:17 +0100

    improve Retrofit error handling
    - move error handling from repository to viewmodel

--- app/src/main/java/com/guilherme/countryapp/data/remote/RemoteRepository.kt
+++ app/src/main/java/com/guilherme/countryapp/data/remote/RemoteRepository.kt
@@ -1,28 +1,12 @@
 package com.guilherme.countryapp.data.remote
 
-import android.util.Log
 import com.guilherme.countryapp.data.mapper.toCountry
 import com.guilherme.countryapp.domain.model.Country
-import retrofit2.HttpException
-import java.io.IOException
 import javax.inject.Inject
 
 class RemoteRepository @Inject constructor(private val api: CountryService) {
-    val TAG = "RemoteRepository"
-
-    suspend fun refreshCountriesFromRemote(): List<Country>  {
-        return try {
-            val result = api.getAllCountries()
-            result.map { it.toCountry() }
-        } catch (e: HttpException) {
-            Log.d(TAG, "Exception: $e")
-            emptyList()
-        } catch (e: IOException) {
-            Log.d(TAG, "Exception: $e")
-            emptyList()
-        } catch (e: Exception) {
-            Log.d(TAG, "Exception: $e")
-            emptyList()
-        }
+    suspend fun refreshCountriesFromRemote(): List<Country> {
+        val result = api.getAllCountries()
+        return result.map { it.toCountry() }
     }
 }
